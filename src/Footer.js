c22b6894d5bfcd8c8e6745730664cc14ab09244a
Cam T. Bui <ncbui@users.noreply.github.com>
2020-10-02 23:35:19 -0700

    Removed alt text

--- src/Footer.js
+++ src/Footer.js
@@ -9,23 +9,11 @@ import portfolioApi from "./Api";
 /**Renders a collection of components */
 export default function Footer() {
 
-  const [info, setInfo] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      let res = await portfolioApi.getContactInfo();
-      setInfo(res);
-    };
-    fetchData();
-  }, []);
-
-
-
   return (
     <div className="Footer fixed-bottom">
       <Container fluid className="Footer-container col-12">
         <div className="Footer-contact col-12 col-md-6 mr-auto text-left">
-        <p className="col-6 mr-auto" alt="Animation as counterbalance to the toggling-animation of the top-right menu. Can be purely for funsies.">
+        <p className="col-6 mr-auto">
         Animated component
         </p>
         </div>
