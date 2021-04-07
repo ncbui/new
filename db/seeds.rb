c814e25f8db6fc756f90237eaf7149fa1f729441
ncbui <ncbui@users.noreply.github.com>
2021-04-06 20:50:18 -0700

    Set up seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+About.create(
+    tagline: "I'm a software developer based in the SF Bay Area. I get a kick out of building tools to help us reshape our inner and outer worlds.",
+    hobbies: "In my spare time I paint and turn leftovers into gold with vermiculture.",
+    firstname: "Cam",
+    lastname: "Bui"
+)
+
+Contact.create(
+    site: 'GitHub',
+    url: 'https://github.com/ncbui'
+)
+Contact.create(
+    site: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/cambui/'
+)
+Contact.create(
+    site: 'CodePen',
+    url: 'https://codepen.io/cambui'
+)
