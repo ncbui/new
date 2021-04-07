b838632a609f2eb5779c4f83c47b00a48ede3d89
ncbui <ncbui@users.noreply.github.com>
2021-04-06 22:24:48 -0700

    Display only if user logged in

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,10 @@
 class ProjectsController < ApplicationController
   before_action :set_project, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, only: [:project, :about]
+
+  def authenticate_user
+
+  end
 
   # GET /projects or /projects.json
   def index
@@ -12,54 +17,64 @@ class ProjectsController < ApplicationController
 
   # GET /projects/new
   def new
-    @project = Project.new
+    if user_signed_in?
+        @project = Project.new
+    end
   end
 
   # GET /projects/1/edit
   def edit
+    if user_signed_in?
+    end
   end
 
   # POST /projects or /projects.json
   def create
-    @project = Project.new(project_params)
+    if user_signed_in?
+        @project = Project.new(project_params)
 
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: "Project was successfully created." }
-        format.json { render :show, status: :created, location: @project }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+        respond_to do |format|
+        if @project.save
+            format.html { redirect_to @project, notice: "Project was successfully created." }
+            format.json { render :show, status: :created, location: @project }
+        else
+            format.html { render :new, status: :unprocessable_entity }
+            format.json { render json: @project.errors, status: :unprocessable_entity }
+        end
+        end
     end
   end
 
   # PATCH/PUT /projects/1 or /projects/1.json
   def update
-    respond_to do |format|
-      if @project.update(project_params)
-        format.html { redirect_to @project, notice: "Project was successfully updated." }
-        format.json { render :show, status: :ok, location: @project }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+    if user_signed_in?
+        respond_to do |format|
+        if @project.update(project_params)
+            format.html { redirect_to @project, notice: "Project was successfully updated." }
+            format.json { render :show, status: :ok, location: @project }
+        else
+            format.html { render :edit, status: :unprocessable_entity }
+            format.json { render json: @project.errors, status: :unprocessable_entity }
+        end
+        end
     end
   end
 
   # DELETE /projects/1 or /projects/1.json
   def destroy
-    @project.destroy
-    respond_to do |format|
-      format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
-      format.json { head :no_content }
+    if user_signed_in?
+        @project.destroy
+        respond_to do |format|
+        format.html { redirect_to projects_url, notice: "Project was successfully destroyed." }
+        format.json { head :no_content }
+        end
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
-      @project = Project.find(params[:id])
+        @project = Project.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
